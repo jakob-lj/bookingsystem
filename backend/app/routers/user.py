8437cbefb46dea6470721e24256120738453ac4c
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-06-22 12:31:35 +0200

    fixed routes to better match codebase

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -22,7 +22,7 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 216000
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token/")
 
 
 class Token(BaseModel):
@@ -103,8 +103,9 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 def me(db: Session=Depends(get_db)):
     db_user = crud.get_user(db, )
 
-@router.post("/token", response_model=Token)
+@router.post("/token/", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    print(form_data)
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
